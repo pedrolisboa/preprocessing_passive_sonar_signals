6ee358f761571679a979612bcc945b144554a423
Pedro Henrique Braga Lisboa <pedrohblisboa@gmail.com>
2020-05-08 12:28:14 -0300

    Changed job creation from system call to another python process to function call from job file. Added cli argument to train_models script to insert job creation script path

--- scripts/train_models.py
+++ scripts/train_models.py
@@ -6,10 +6,10 @@ from scipy.signal import decimate, spectrogram
 
 from sklearn.model_selection import StratifiedKFold
 from itertools import product
-
-import lps_maestro as maestro
+from subprocess import Popen
 
 import argparse
+import sys
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -27,9 +27,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-c','--useCluster', action='store', type=str2bool,
         dest='useCluster', required = True, default=False,
             help = "Use lps cluster to run the config.")
+parser.add_argument('-f','--jobfileFolder', action='store',
+        dest='jobfileFolder', required = False, default='./container',
+            help = "Point to where the job creation script is located. Only used in local mode")
 args = parser.parse_args()
 
 if not args.useCluster:
+    sys.path.append(args.jobfileFolder)
+    import create_job
+
+
     base_dir = os.getcwd()
     configs_folder = os.listdir('configs')[1]
     config_folder = os.path.join('configs', configs_folder)
@@ -41,12 +48,16 @@ if not args.useCluster:
     datapath = os.path.join('data', 'raw', dataset, 
                            '%s_acoustic_lane_fs_22050_b_8_pos_45m.hdf5' % dataset)
 
-
+    
     for filename in mode_combinations:
         filepath = os.path.abspath(os.path.join(config_folder, filename))
         modelpath = os.path.join(models_dir, filename.replace('.json', ''))
-        os.system("python3 ./container/create_job.py -c %s -o %s -d %s" % (filepath, modelpath, datapath))
+        # process = Popen("python3 %s -c %s -o %s -d %s" % (jobfile, filepath, modelpath, datapath), shell=True).wait()
+        create_job.run_jobs(filepath, datapath, modelpath)
+        # print(process)
 else:
+    import lps_maestro as maestro
+
     username = 'plisboa'
     taskname = 'user.plisboa.acoustic_lane.4classes.preprocessing_passive_sonar'
     dataset = 'user.plisboa.4classes_acoustic_lane_fs_22050_b_8_pos_45m.hdf5'
